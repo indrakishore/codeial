c2505364bc9c1e4c13cd15032eca847111c16b6a
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2022-05-09 15:20:13 +0530

    Set up sign-in and sign-up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,3 +9,20 @@ module.exports.profile = function(req, res){
         title : "profile"
     });
 }
+
+// Actions
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+//routed signIn and signUp pages
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
+
 module.exports = router;
 
 router.use('/users', require('./users'));
@@ -12,6 +16,6 @@ router.use('/users', require('./users'));
 //for any further routes, access from here
 //router.use('/routerName', require('./routerfile'));
 
-module.exports = router;
+// module.exports = router;
 
 
