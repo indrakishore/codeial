7fb03c423217769f1f970473ee77b86af407f1ea
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2022-05-12 16:48:09 +0530

    set up SASS

--- index.js
+++ index.js
@@ -43,6 +43,17 @@ const passportLocal = require('./config/passport-local-strategy');
 // MongoStore
 const MongoStore = require('connect-mongo');
 
+//sass-middleware
+const sassMiddleware = require('node-sass-middleware');
+
+
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest : './assets/css',
+    debug : true,
+    outputStyle : 'extended',
+    prefix : '/css'
+}));
 
 app.use(express.urlencoded());
 app.use(cookieParser());
