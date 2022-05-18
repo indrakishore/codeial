131134188083aacb362a602dd17db4ae07be95b7
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2022-05-18 16:09:47 +0530

    Shown up post content with user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,16 +1,33 @@
 //exports a function which is publically available in route files
 //commented bcz something sent directly from the browser
-
+const { populate } = require('../models/post');
+const Post = require('../models/post');
  
 module.exports.home = function(req, res){
     // return res.end('<h1> Express is up for codeial! </h1>');
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // cookies going back as response
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title : "Home"
+    // res.cookie('user_id', 25);
+
+    // find all the message
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title : "codeial | Home",
+    //         posts : posts,
+
+    //     })
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, post){
+        return res.render('home', {
+            title : "codeial | Home",
+            posts : post,
+
+        })
     });
 }
+
 
 // module.exports.actionName = function(req, res){
     
