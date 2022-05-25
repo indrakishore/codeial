b0097ef82508678e32a623e41f142cb7dffbe58e
Indra Kishore Kumar <indrakishore.mca19.du@gmail.com>
2022-05-25 15:07:52 +0530

    Added flash message to more action

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,17 +5,19 @@ const Comment = require('../models/comment');
 // Convert in async await
 module.exports.create = async function (req, res) {
     try {
-        
-            await Post.create({
-                content: req.body.content,
-                user: req.user._id
-            });
 
-            return res.redirect('back');
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+
+        req.flash('success', 'Post published');
+        return res.redirect('back');
 
     } catch (err) {
-        console.log("Error", err);
-        return;
+        // console.log("Error", err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
@@ -44,13 +46,15 @@ module.exports.destroy = async function (req, res) {
             post.remove();
             // deleting the comment associated with
             await Comment.deleteMany({ post: req.params.id });
+            req.flash('success', 'Post and associated comments deleted');
             return res.redirect('back');
         } else {
+            req.flash('error', 'You cannot delete this post');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log("Error", err);
-        return;
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
